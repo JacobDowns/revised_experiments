945bc93348e0a1ce65ac74481f2df03d039c1e1b
Jacob Downs <jacob.downs@umontana.edu>
2017-03-20 20:39:50 -0600

    ASdf

---
+++ sims/reference_channel/plot/write_data.py
@@ -0,0 +1,68 @@
+from time_view import *
+from pylab import *
+from constants import *
+from tr_plot import *
+
+"""
+Write data to text files for plotting -- speeds things up when lots of plot 
+tweaks are needed. 
+"""
+
+### Get data
+
+view1 = TimeView('../hdf5_results/out.hdf5')
+ff = FacetFunctionDouble(view1.mesh)
+p = TRPlot(view1.mesh)
+out = File('S/S.pvd')
+out_h = File('S/h.pvd')
+
+for i in range(view1.num_steps):
+  print i
+  p.copy_tr_to_facet(view1.get_S(i), ff)
+  out << ff
+  out_h << view1.get_h(i)
+
+quit()
+
+ts = view1.get_ts() / pcs['spm']
+
+# Pressure at points
+pfos1 = view1.get_pfo_array_at_points([10e3, 20e3, 50e3], [10e3, 10e3, 10e3])
+#pfos2 = view2.get_pfo_array_at_points([10e3, 20e3, 50e3], [10e3, 10e3, 10e3])
+
+# Average pressure
+avg_pfos1 = view1.get_avg_pfo_array()
+#avg_pfos2 = view2.get_avg_pfo_array()
+
+# Average melt
+avg_ms1 = view1.get_avg_m_array()
+#avg_ms2 = view2.get_avg_m_array()
+
+# Average sheet height
+avg_hs1 = view1.get_avg_h_array()
+#avg_hs2 = view2.get_avg_h_array()
+
+# Average sliding velocity 
+avg_ubs1 = view1.get_avg_u_b_array()
+#avg_ubs2 = view2.get_avg_u_b_array()
+
+# Average conductivity
+avg_ks1 = view1.get_avg_k_array()
+#avg_ks2 = view2.get_avg_k_array()
+
+
+### Write data to text files
+
+savetxt('ts.txt', ts)
+savetxt('pfos1.txt', pfos1)
+#savetxt('pfos2.txt', pfos2)
+savetxt('avg_pfos1.txt', avg_pfos1)
+#savetxt('avg_pfos2.txt', avg_pfos2)
+savetxt('avg_ms1.txt', avg_ms1 * pcs['spy'])
+#savetxt('avg_ms2.txt', avg_ms2 * pcs['spy'])
+savetxt('avg_hs1.txt', avg_hs1)
+#savetxt('avg_hs2.txt', avg_hs2)
+savetxt('avg_ubs1.txt', avg_ubs1 * pcs['spy'])
+#savetxt('avg_ubs2.txt', avg_ubs2 * pcs['spy'])
+savetxt('avg_ks1.txt', avg_ks1)
+#savetxt('avg_ks2.txt', avg_ks2)
