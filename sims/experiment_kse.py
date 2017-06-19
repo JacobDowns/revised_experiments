6c258028a126db86b752507b0560947a35885c5b
Jacob Downs <jacob.downs@umontana.edu>
2017-06-19 17:47:42 -0600

    asdf

--- sims/experiment_kse.py
+++ sims/experiment_kse.py
@@ -3,13 +3,13 @@ from experiment import *
 kse_experiment = Experiment('kse')
 
 ### Steady state
-run1 = conductivity_experiment.add_run('steady', '../inputs/synthetic/inputs_flat_high.hdf5', steady = True)
+run1 = kse_experiment.add_run('steady', '../inputs/synthetic/inputs_flat_high.hdf5', steady = True)
 run1.run_options['vark'] = True
 run1.run_options['k_bound_low'] = 1e-3
 run1.run_options['k_bound_high'] = 8e-3
 
 ### Winter
-run2 = conductivity_experiment.add_run('winter', run1.model_inputs['steady_file'], steady = False)
+run2 = kse_experiment.add_run('winter', run1.model_inputs['steady_file'], steady = False)
 run2.run_options['vark'] = True
 run2.run_options['scale_k'] = True
 run2.run_options['scale_k_max'] = run1.run_options['scale_k_max']
