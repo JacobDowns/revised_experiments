6f28b756d7f401c01297817c8faee37b685b7c26
Jacob Downs <jacob.downs@umontana.edu>
2017-06-20 09:42:38 -0600

    sdf

--- sims/experiment_kse.py
+++ sims/experiment_kse.py
@@ -10,7 +10,7 @@ run1.run_options['k_bound_high'] = 8e-3
 run1.run_options['k_scale_max'] = 0.00458806211204
 
 ### Winter
-run2 = kse_experiment.add_run('winter', run1.model_inputs['steady_file'], steady = False)
+run2 = kse_experiment.add_run('winter', run1.model_inputs['steady_file'] + '.hdf5', steady = False)
 run2.run_options['vark'] = True
 run2.run_options['scale_k'] = True
 run2.run_options['scale_k_max'] = run1.run_options['scale_k_max']
