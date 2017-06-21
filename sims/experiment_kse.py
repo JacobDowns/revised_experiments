ba6d6d54f78988a472a18d1214d2511e22083b13
Jacob Downs <jacob.downs@umontana.edu>
2017-06-21 17:46:11 -0600

    asdf

--- sims/experiment_kse.py
+++ sims/experiment_kse.py
@@ -14,3 +14,11 @@ run2 = kse_experiment.add_run('winter', run1.model_inputs['steady_file'] + '.hdf
 run2.run_options['vark'] = True
 run2.run_options['scale_k'] = True
 run2.run_options['scale_k_max'] = run1.run_options['scale_k_max']
+
+
+### Winter with a small storage term
+run3 = kse_experiment.add_run('bonus', run1.model_inputs['steady_file'] + '.hdf5', steady = False)
+run3.run_options['vark'] = True
+run3.run_options['scale_k'] = True
+run3.model_inputs['constants']['e_v'] = 1e-8  
+run3.run_options['scale_k_max'] = run1.run_options['scale_k_max']
