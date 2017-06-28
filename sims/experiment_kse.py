5294225be12a6712715575ede3ea75d0a9f3c9a4
Jacob Downs <jacob.downs@umontana.edu>
2017-06-28 14:12:48 -0600

    asf

--- sims/experiment_kse.py
+++ sims/experiment_kse.py
@@ -12,7 +12,7 @@ run1 = kse_experiment.add_run('steady', '../inputs/synthetic/inputs_trough_high.
 run1.run_options['vark'] = True
 run1.run_options['k_bound_low'] = 4e-3
 run1.run_options['k_bound_high'] = 8e-3
-run1.run_options['scale_k_min'] = 4e-8
+run1.run_options['scale_k_min'] = 5e-8
 run1.run_options['scale_k_max'] = 0.005527864045
 
  
