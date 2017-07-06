fc1240e6680e7eca2f24cfdfb56831c9057dba79
Jacob Downs <jacob.downs@umontana.edu>
2017-07-06 14:55:53 -0600

    saf

--- sims/experiment_cse.py
+++ sims/experiment_cse.py
@@ -19,5 +19,12 @@ run1.run_options['tune_atol'] = 1e-8
 run2 = cse_experiment.add_run('winter', run1.model_inputs['steady_file'] + '.hdf5', steady = False)
 run2.run_options['scale_k_max'] = run1.run_options['scale_k_max']
 run2.model_inputs['use_channels'] = True
+
+### Trough winter1
+run3 = cse_experiment.add_run('winter1', run1.model_inputs['steady_file'] + '.hdf5', steady = False)
+run3.run_options['scale_k_max'] = run1.run_options['scale_k_max']
+run3.model_inputs['use_channels'] = True
+run3.run_options['scale_m_min'] = 1e-9
+  
   
   
