588225cf1dc1c2f862e9492e380b9e62c396f009
Jacob Downs <jacob.downs@umontana.edu>
2017-07-17 17:19:03 -0600

    asdf

--- sims/experiment_cse.py
+++ sims/experiment_cse.py
@@ -9,10 +9,12 @@ spd = sim_constants['spd']
 run1 = cse_experiment.add_run('steady', '../inputs/synthetic/inputs_trough_high.hdf5', steady = True)
 run1.run_options['k_bound_low'] = 1e-4
 run1.run_options['k_bound_high'] = 2e-4
-run1.run_options['scale_k_max'] = 0.000138196601125
-run1.run_options['end_time'] = 1000.0*spd
+run1.run_options['scale_k_max'] = 7e-4
+run1.run_options['end_time'] = 120.0*spd
 run1.model_inputs['use_channels'] = True
 run1.run_options['tune_atol'] = 1e-8
+run1.run_options['dt'] = spd / 64
+
 
 ### Trough winter
 run2 = cse_experiment.add_run('winter', run1.model_inputs['steady_file'] + '.hdf5', steady = False)
@@ -24,14 +26,7 @@ run2.model_inputs['use_channels'] = True
 run3 = cse_experiment.add_run('winter1', run1.model_inputs['steady_file'] + '.hdf5', steady = False)
 run3.run_options['scale_k_max'] = run1.run_options['scale_k_max']
 run3.model_inputs['use_channels'] = True
-run3.run_options['scale_m_min'] = 1e-9
-
-
-### Trough winter 2 for basal melt experiment
-run4 = cse_experiment.add_run('winter2', run1.model_inputs['steady_file'] + '.hdf5', steady = False)
-run4.run_options['scale_k_max'] = run1.run_options['scale_k_max']
-run4.model_inputs['use_channels'] = True
-run4.run_options['scale_m_min'] = 1e-9
-run4.model_inputs['constants']['e_v'] = 1e-3  
+# 1cm basal melt
+run3.run_options['scale_m_min'] = 3.171e-10
 
   
